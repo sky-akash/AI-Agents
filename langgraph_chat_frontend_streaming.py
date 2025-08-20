f00d95c82e032894ef31de1a9662f2e68cf4c627
Akash Mittal <67840689+sky-akash@users.noreply.github.com>
2025-08-20 16:33:29 +0200

    generator

--- langgraph_chat_frontend_streaming.py
+++ langgraph_chat_frontend_streaming.py
@@ -39,7 +39,7 @@ if user_input:
     with st.chat_message('assistant'):
 
         ai_message = st.write_stream(
-            message_chunk.content for message_chunk, metadata in chatbot.stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(     # python generator
                 {'messages': [HumanMessage(content=user_input)]},
                 config = {'configurable': {'thread_id': 'thread-1'}},
                 stream_mode = 'messages'
