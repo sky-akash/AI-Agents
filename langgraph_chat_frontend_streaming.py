45347f7a3ebcb97a794d3e87c16af9547cbe8ca2
Akash Mittal <67840689+sky-akash@users.noreply.github.com>
2025-08-20 16:32:54 +0200

    streamlit + chatbot + streaming

--- langgraph_chat_frontend_streaming.py
+++ langgraph_chat_frontend_streaming.py
@@ -36,16 +36,32 @@ if user_input:
         st.text(user_input)
 
 
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=config)       # need input as initial state 
-    ai_message = response['messages'][-1].content
+    with st.chat_message('assistant'):
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config = {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode = 'messages'
+            )
+        )
 
     st.session_state['message_history'].append({'role':'assistant', 'content': ai_message})
-    with st.chat_message('asisstant'):
-        st.text(ai_message)
 
-    # st.session_state['message_history'].append({'role':'assistant', 'content': user_input})
-    # with st.chat_message('asisstant'):
-    #     st.text(user_input)
+
+
+
+# Need to replace invoke with stream :)
+    # response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=config)       # need input as initial state 
+    # ai_message = response['messages'][-1].content
+
+    # st.session_state['message_history'].append({'role':'assistant', 'content': ai_message})
+    # with st.chat_message('asistant'):
+    #     st.text(ai_message)
+
+    # # st.session_state['message_history'].append({'role':'assistant', 'content': user_input})
+    # # with st.chat_message('asistant'):
+    # #     st.text(user_input)
 
 
 
